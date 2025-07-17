61cd39ad229851d3b87a6a2be1a9840d814bb3a1
Torrin Conrath <torrin@vt.edu>
2025-07-17 00:15:07 -0400

    finished adding setup and fixed export_config

--- env/export_config.py
+++ env/export_config.py
@@ -6,5 +6,8 @@ import os
 # Output to the React frontend's public folder (adjust as needed)
 output_path = os.path.join(os.path.dirname(__file__), '../CarScorePredictor/config/config.json')
 
+# Make dir if needed
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
 with open(output_path, 'w') as f:
     json.dump(config, f, indent=2)
